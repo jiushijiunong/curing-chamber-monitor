d9ab161c0bb162a9eea33ca4c2573421716f669a
jiushijiunong <51561504@qqcom>
2018-03-18 21:59:56 +0800

    add logging, email notification and samples data retrieving logic

---
+++ pocket_monitor/mo/soap_response.py
@@ -0,0 +1,59 @@
+from collections import OrderedDict
+
+try:
+    import xml.etree.cElementTree as ele_tree
+except ImportError:
+    import xml.etree.ElementTree as ele_tree
+
+
+class SoapResponse(object):
+    def __init__(self, rep_code=None, rep_content=None):
+
+        self.rep_code = rep_code
+        self.rep_content = rep_content
+
+    def get_response_entities(self, entity_root_name):
+        root = ele_tree.fromstring(self.rep_content)
+
+        # find entity root
+        entity_root = self._match_child_node(root, entity_root_name)
+
+        if entity_root is None:
+            return
+
+        ls_ret = []
+        if self._has_child(entity_root):
+            for child in entity_root:
+                ret = self._get_entity(child)
+                if ret:
+                    ls_ret.append(ret)
+        else:
+            ls_ret.append(entity_root.text)
+        return ls_ret
+
+    def _has_child(self, node):
+        for child in node:
+            if child:
+                return True
+        return False
+
+    def _get_entity(self, node):
+        ret = OrderedDict()
+        for child in node:
+            child_name = self._get_node_tag_name(child)
+            if self._has_child(child):
+                ret[child_name] = self._get_entity(child)
+            else:
+                ret[child_name] = child.text
+        return ret
+
+    def _get_node_tag_name(self, node):
+        arr = node.tag.split("}")
+        return arr[-1]
+
+    def _match_child_node(self, node, child_node_name):
+        for child in node:
+            if self._get_node_tag_name(child) == child_node_name:
+                return child
+            else:
+                return self._match_child_node(child, child_node_name)
--- pocket_monitor/urls.py
+++ pocket_monitor/urls.py
@@ -15,10 +15,15 @@ Including another URLconf
 """
 from django.conf.urls import url,include
 from django.contrib import admin
+from rest_framework.documentation import include_docs_urls
 from pocket_monitor.db import urls as rest_urls
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    # disable django default admin pages
+    # url(r'^admin/', admin.site.urls),
+
+
     # url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    
     url(r'^', include(rest_urls)),
 ]
